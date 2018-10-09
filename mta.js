8a3bbeada4fb2115c0f2dd694ee819f16776a89c
Jonathan Rockway <jon@jrock.us>
2018-10-08 21:11:55 -0400

    provide some visual indication that the data is recent, though the dates of the A/C/E feed seem to be in the future sometimes ;)

--- mta.js
+++ mta.js
@@ -1,22 +1,58 @@
 "use strict";
 
+var nextid = 0;
+var lastUpdated = new Array();
+
+function updateLastUpdated() {
+    var len = lastUpdated.length;
+    var now = Date.now();
+    var diff = 0;
+    var id = 0;
+    for(var i = 0; i < len-1; i++) {
+        diff = now - lastUpdated[i];
+        id = i * 3;
+        if (diff > 0 && diff < 10000) {
+            $(`#times tr:eq(${id})`).addClass("new");
+            $(`#times tr:eq(${id})`).removeClass("old");
+        } else if (diff > 60000 || diff < 0) {
+            $(`#times tr:eq(${id})`).removeClass("new");
+            $(`#times tr:eq(${id})`).addClass("old");
+        } else {
+            $(`#times tr:eq(${id})`).removeClass("old");
+            $(`#times tr:eq(${id})`).removeClass("new");
+        }
+    }
+    diff = now - lastUpdated[len-1];
+    if (diff > 0 && diff < 30000) {
+        $("#status").addClass("new");
+        $("#status").removeClass("old");
+    } else if (diff > 60000 || diff < 0) {
+        $("#status").removeClass("new");
+        $("#status").addClass("old");
+    } else {
+        $("#status").removeClass("new");
+        $("#status").removeClass("old");
+    }
+}
+
 function drawTime(data, status, xhr) {
     var id = xhr.id;
-    $(`#times tr:eq(${id})`).html(`<td colspan=2 class="station">${data.stationName}</td>`);
-    $(`#times tr:eq(${id+1})`).html(`<td>${data.direction1.name}</td><td>${data.direction1.times.map(time => `<span class="route route-${time.route}">${time.route}</span> ${time.minutes}`).join(" ")}</td>`);
-    $(`#times tr:eq(${id+2})`).html(`<td>${data.direction2.name}</td><td>${data.direction2.times.map(time => `<span class="route route-${time.route}">${time.route}</span> ${time.minutes}`).join(" ")}</td>`);
+    $(`#times tr:eq(${3*id})`).html(`<td colspan=2 class="station">${data.stationName}</td>`);
+    $(`#times tr:eq(${3*id+1})`).html(`<td>${data.direction1.name}</td><td>${data.direction1.times.map(time => `<span class="route route-${time.route}">${time.route}</span> ${time.minutes}`).join(" ")}</td>`);
+    $(`#times tr:eq(${3*id+2})`).html(`<td>${data.direction2.name}</td><td>${data.direction2.times.map(time => `<span class="route route-${time.route}">${time.route}</span> ${time.minutes}`).join(" ")}</td>`);
+    lastUpdated[id] = new Date(data.lastUpdatedOn*1000);
 }
 
 function drawTimeError(xhr, status, error) {
     var id = xhr.id;
-    if ($(`#times tr:eq(${id})`).html() == "") {
-        $(`#times tr:eq(${id})`).html(`<td colspan=2 class="station">unknown</td>`);
-        $(`#times tr:eq(${id+1})`).html(`<td>${status}</td><td></td>`);
-        $(`#times tr:eq(${id+2})`).html(`<td>${error}</td><td></td>`);
+    if ($(`#times tr:eq(${3*id})`).html() == "") {
+        $(`#times tr:eq(${3*id})`).html(`<td colspan=2 class="station">unknown</td>`);
+        $(`#times tr:eq(${3*id+1})`).html(`<td>${status}</td><td></td>`);
+        $(`#times tr:eq(${3*id+2})`).html(`<td>${error}</td><td></td>`);
+    } else {
+        lastUpdated[id] = new Date(0);
     }
 }
-
-var nextid = 0;
 
 function request(url, first) {
     if (first) {
@@ -28,7 +64,7 @@ function request(url, first) {
         success: drawTime,
         error: drawTimeError,
         timeout: 4500,
-    })["id"] = nextid * 3;
+    })["id"] = nextid;
     nextid++;
 }
 
@@ -50,11 +86,14 @@ function drawStatus(data, st, xhr) {
             }
         }
     }
+    lastUpdated[xhr.id] = new Date(data.lastUpdated);
 }
 
 function drawStatusError(xhr, status, error) {
     if ($("#status").html() == "") {
         $("#status").html(`Loading status: ${status} (${error})`);
+    } else {
+        lastUpdated[xhr.id] = new Date(0);
     }
 }
 
@@ -65,7 +104,8 @@ function status(url) {
         success: drawStatus,
         error: drawStatusError,
         timeout: 4500,
-    });
+    })["id"] = nextid;
+    nextid++;
 }
 
 function load(first) {
@@ -79,5 +119,8 @@ function load(first) {
 
     status("https://collector-otp-prod.camsys-apps.com/realtime/serviceStatus?apikey=qeqy84JE7hUKfaI0Lxm2Ttcm6ZA0bYrP");
 
+    if (first) {
+        window.setInterval(updateLastUpdated, 1000);
+    }
     window.setTimeout(() => load(false), 5000);
 }
